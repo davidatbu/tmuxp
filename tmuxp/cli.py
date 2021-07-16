3fe3a0fae1fa6d35e1282f45ad5359ba2af22b08
David Assefa Tofu <davidat@bu.edu>
2021-07-16 11:12:24 +0300

    Add --quiet to freeze

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -913,8 +913,9 @@ def command_shell(
 @click.option('-f', '--config-format', type=click.Choice(['yaml', 'json']), help='format to save in')
 @click.option('-o', '--save-to', type=click.Path(exists=False), help='file to save to')
 @click.option('-y', '--yes', type=bool, is_flag=True, default=False, help="Don't prompt for confirmation")
+@click.option('-q', '--quiet', type=bool, is_flag=True, default=False, help="Don't prompt for confirmation")
 @click.option('--force', 'force', help='overwrite the config file', is_flag=True)
-def command_freeze(session_name, socket_name, config_format, save_to, socket_path, yes, force):
+def command_freeze(session_name, socket_name, config_format, save_to, socket_path, yes, quiet, force):
     """Snapshot a session into a config.
 
     If SESSION_NAME is provided, snapshot that session. Otherwise, use the
@@ -939,19 +940,21 @@ def command_freeze(session_name, socket_name, config_format, save_to, socket_pat
     newconfig = config.inline(sconf)
     configparser.import_config(newconfig)
 
-    print(
-        '---------------------------------------------------------------'
-        '\n'
-        'Freeze does its best to snapshot live tmux sessions.\n'
-    )
+    if not quiet:
+        print(
+            '---------------------------------------------------------------'
+            '\n'
+            'Freeze does its best to snapshot live tmux sessions.\n'
+        )
     if not (yes or click.confirm(
         'The new config *WILL* require adjusting afterwards. Save config?'
     )):
-        print(
-            'tmuxp has examples in JSON and YAML format at '
-            '<http://tmuxp.git-pull.com/examples.html>\n'
-            'View tmuxp docs at <http://tmuxp.git-pull.com/>.'
-        )
+        if not quiet:
+            print(
+                'tmuxp has examples in JSON and YAML format at '
+                '<http://tmuxp.git-pull.com/examples.html>\n'
+                'View tmuxp docs at <http://tmuxp.git-pull.com/>.'
+            )
         sys.exit()
 
     dest = save_to
@@ -996,17 +999,8 @@ def command_freeze(session_name, socket_name, config_format, save_to, socket_pat
         buf.write(newconfig)
         buf.close()
 
-        print('Saved to %s.' % dest)
-
-    if config_format == 'yaml':
-        newconfig = configparser.export(
-            'yaml', indent=2, default_flow_style=False, safe=True
-        )
-    elif config_format == 'json':
-        newconfig = configparser.export('json', indent=2)
-    else:
-        sys.exit('Unknown config format.')
-
+        if not quiet:
+            print('Saved to %s.' % dest)
 
 
 @cli.command(name='load', short_help='Load tmuxp workspaces.')
